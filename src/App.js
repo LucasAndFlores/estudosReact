bd1291967aebb74379bab5355c89b54c2135e03c
lucasandflores <lcs.oliveira93@gmail.com>
2021-09-24 15:39:50 -0300

    aula hooks state e effect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [ person, setPerson ] = useState()
+  const [peopleApi, setPeopleApi] = useState([])
+
+
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/${person}/`).then((response) => {
+      console.log(response.data.results)
+      setPeopleApi(response.data.results)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [person])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => {setPerson("people")}}> Buscar pessoas na API star wars </button>
+      <button onClick={() => {setPerson("vehicles")}}> Buscar carros na API star wars </button>
+      <button onClick={() => {setPerson("planets")}}> Buscar planetas na API star wars </button>
+      <div> {peopleApi.map(item => {
+          return <ul> {JSON.stringify(item.name)}</ul>
+      })} </div>
     </div>
   );
 }
